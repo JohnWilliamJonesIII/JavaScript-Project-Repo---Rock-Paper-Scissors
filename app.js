57bc622b7a440a5caebf90fb67976885e397d497
John William Jones III <johnwilliamjonesiii@gmail.com>
2022-03-02 13:16:27 -0600

    Added HTML to display buttons, created class

---
+++ app.js
@@ -0,0 +1,4 @@
+const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choice')
+const actualResult = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('rpsButton')
