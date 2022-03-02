e706025b6d8804283cf734283ed1618424b79989
John William Jones III <johnwilliamjonesiii@gmail.com>
2022-03-02 13:31:25 -0600

    Removed rpsButton class for redundancy, added functionality

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
-const actualResult = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('rpsButton')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const actualResultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+
+}))
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
